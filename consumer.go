52bd4d71501ed57b8f9891d5f6bbfd57b56db134
Rafał Gałus <rafal@mycujoo.tv>
2019-10-17 16:01:10 +0200

    Add Textual to ConsumerMessage and error handling (#8)

--- consumer.go
+++ consumer.go
@@ -2,6 +2,7 @@ package kafkaavro
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -19,8 +20,12 @@ type Consumer struct {
 type ConsumerMessage struct {
 	*kafka.Message
 	Error error
+
 	// Message Value parsed into maps/structs
 	Parsed interface{}
+
+	// JSON representation of the message
+	Textual []byte
 }
 
 // NewConsumer is a basic consumer to interact with schema registry, avro and kafka
@@ -70,17 +75,64 @@ func (ac *Consumer) Messages(stopChan chan struct{}) (chan ConsumerMessage, chan
 
 				switch e := ev.(type) {
 				case *kafka.Message:
+					parsed, textual, err := ac.decodeAvroBinary(e.Value)
+
+					if err != nil {
+						output <- ConsumerMessage{
+							Message: e,
+							Error:   err,
+						}
+
+						continue
+					}
+
 					msg := ConsumerMessage{
 						Message: e,
+						Parsed:  parsed,
+						Textual: textual,
 					}
-					msg.Parsed, msg.Error = ac.decodeAvroBinary(e.Value)
-					output <- msg
+
+					if _, err = json.Marshal(msg.Parsed); err != nil {
+						msg.Error = err
+						output <- msg
+
+						continue
+					}
+
+					if e.TopicPartition.Topic == nil {
+						msg.Error = errors.New("null topic")
+						output <- msg
+
+						continue
+					}
+
+					if msg.Parsed == nil {
+						msg.Error = errors.New("missing parsed data")
+						output <- msg
+
+						continue
+					}
+
+					if _, ok := msg.Parsed.(map[string]interface{}); !ok {
+						msg.Error = errors.New("parsed data is wrong type")
+						output <- msg
+
+						continue
+					}
+
+					output <- ConsumerMessage{
+						Message: e,
+						Parsed:  parsed,
+						Textual: textual,
+					}
+
 				default:
 					other <- e
 				}
 			}
 		}
 	}()
+
 	return output, other
 }
 
@@ -93,21 +145,28 @@ func (ac *Consumer) Close() {
 	close(ac.stopChan)
 }
 
-func (ac *Consumer) decodeAvroBinary(data []byte) (interface{}, error) {
+func (ac *Consumer) decodeAvroBinary(data []byte) (interface{}, []byte, error) {
 	if data[0] != 0 {
-		return nil, errors.New("invalid magic byte")
+		return nil, nil, errors.New("invalid magic byte")
 	}
 	schemaId := binary.BigEndian.Uint32(data[1:5])
 	codec, err := ac.schemaRegistryClient.GetSchemaByID(int(schemaId))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	// Convert binary Avro data back to native Go form
 	native, _, err := codec.NativeFromBinary(data[5:])
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return native, err
+
+	textual, err := codec.TextualFromNative(nil, native)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return native, textual, err
 }
 
 // EnsureTopics returns error if one of the consumed topics
